37fbe8e9b48166bc396d9c97001f0d0404f5aa9f
Mokhamad Rofiudin <mokh.rofiudin@gmail.com>
2016-06-26 01:08:11 +0700

    add configurations

--- config/livecms.php
+++ config/livecms.php
@@ -1,6 +1,46 @@
 <?php
 
 return [
+    
+    'thumbnailer' => [
+
+        // 'thumb' => '_thumb_',
+
+        // 'size' => '300x300',
+    ],
+
+    'uploader' => [
+        
+        // 'override' => false,
+
+        // 'modelOverride' => true,
+
+        // 'baseFolder' => 'public/uploads',
+
+        // 'folder' => '',
+
+    ],
+
+    'emailer' => [
+
+        'from' => [
+            'address' => env('EMAIL_ADDRESS', null),
+            'name' => env('EMAIL_NAME', null),
+        ],
+        'subject' => 'Hello',
+        'siteurl' => env('SITE_URL', env('APP_URL')),
+        'sitename' => env('SITE_NAME', null),
+        'siteslogan' => env('SITE_SLOGAN', null),
+
+        'activation' => ['subject' => 'Account Activation'],
+
+        'welcome' => [],
+
+        'alert' => ['parent' => 'welcome', 'subject' => 'alert'],
+    
+        'invoice' => [],
+    ],
+
     'domain' => env('APP_DOMAIN', 'livecms.dev'),
 
     'slugs' => [
