0ba7b47219ce59ac00a9414f74842e014f21307c
Mokhamad Rofiudin <mokh.rofiudin@gmail.com>
2016-10-02 18:27:35 +0700

    fix static form

--- views/admin/staticpage/form.blade.php
+++ views/admin/staticpage/form.blade.php
@@ -5,27 +5,8 @@
     {!! LiveCMS\FormBuilder\FormBuilder::model(${$base}, $groupName, null, function ($builder) use ($staticpage, $parents) {
         $builder->setField('permalink', 'content');
         $builder->setField('url', 'permalink');
-        $builder->addCustomField('permalink', '
-            <div class="row form-group">
-                <div class="col-md-2">'.
-                    Form::label('permalink', trans('livecms::livecms.permalink'), ['class' => 'control-label']).'
-                </div>
-                <div class="col-md-10">'.
-                    Form::text('permalink', $staticpage->permalink ? $staticpage->permalink->permalink : '', ['class' => 'form-control', 'placeholder' => url('path/sebagai/permalink')]).'
-                </div>
-            </div>
-        ');
-        $builder->addCustomField('parent', '
-            <div class="row form-group">
-                <div class="col-md-2">'.
-                    Form::label('parent', trans('livecms::livecms.parent'), ['class' => 'control-label']).'
-                </div>
-
-                <div class="col-md-10">'.
-                    Form::select('parent', $parents, $staticpage->parent_id, ['class' => 'form-control']).'
-                </div>
-            </div>
-        ');
+        $builder->addCustomField('permalink', Form::text('permalink', $staticpage->permalink ? $staticpage->permalink->permalink : '', ['class' => 'form-control', 'placeholder' => url('path/sebagai/permalink')]));
+        $builder->addCustomField('parent', Form::select('parent', $parents, $staticpage->parent_id, ['class' => 'form-control']));
     }) !!}
 @stop
 
