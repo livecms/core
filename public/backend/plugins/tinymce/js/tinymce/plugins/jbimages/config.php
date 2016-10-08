f95b0b6e56143d4b68f1decb2c70a81d4731fbf8
Mokhamad Rofiudin <mokh.rofiudin@gmail.com>
2016-10-08 15:23:37 +0700

    update tinymce plugins config

--- public/backend/plugins/tinymce/js/tinymce/plugins/jbimages/config.php
+++ public/backend/plugins/tinymce/js/tinymce/plugins/jbimages/config.php
@@ -38,7 +38,7 @@
 | -------------------------------------------------------------------*/
 
 	
-	$config['img_path'] = '/uploads'; // Relative to domain name
+	$config['img_path'] = '/uploads/images'; // Relative to domain name
 	$config['upload_path'] = $_SERVER['DOCUMENT_ROOT'] . $config['img_path']; // Physical path. [Usually works fine like this]
 
 
