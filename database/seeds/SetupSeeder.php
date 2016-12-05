9ab10acab496d068be7d1aec45f76a55326461ac
Mokhamad Rofiudin <mokh.rofiudin@gmail.com>
2016-12-06 02:11:14 +0700

    dont delete existing value in seeder

--- database/seeds/SetupSeeder.php
+++ database/seeds/SetupSeeder.php
@@ -11,9 +11,7 @@ class SetupSeeder extends Seeder
      */
     public function run()
     {
-        DB::table('settings')->delete();
-
-        DB::table('settings')->insert([
+        $datas = [
             ['publicable' => true,'key' => 'site_name', 'value' => 'Live CMS'],
             ['publicable' => true,'key' => 'site_initial', 'value' => 'LC'],
             ['publicable' => true,'key' => 'site_email', 'value' => 'no.reply@livecms.dev'],
@@ -22,6 +20,14 @@ class SetupSeeder extends Seeder
             ['publicable' => true,'key' => 'slug_staticpage', 'value' => 'p'],
             ['publicable' => true,'key' => 'mailchimp_key', 'value' => ''],
             ['publicable' => true,'key' => 'google_analytic_id', 'value' => ''],
-        ]);
+        ];
+
+        foreach ($datas as $data) {
+            $exist = DB::table('settings')->where(array_only($data, ['publicable', 'key']))->first();
+            if (!$exist) {
+                DB::table('settings')->insert($data);
+            }
+            
+        }
     }
 }
