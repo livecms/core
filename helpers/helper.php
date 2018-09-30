3c17a527359e9a267edc7477383eec14ae440d93
Mokhamad Rofiudin <mokh.rofiudin@gmail.com>
2018-09-30 17:16:49 +0700

    update helper

--- helpers/helper.php
+++ helpers/helper.php
@@ -1,6 +1,8 @@
 <?php
 
-const LIVECMS = 'livecms';
+if (! defined('LIVECMS')) {
+    define('LIVECMS', 'livecms');
+}
 
 if (! function_exists('LC_CurrentInstance')) {
     function LC_CurrentInstance()
@@ -10,15 +12,58 @@ if (! function_exists('LC_CurrentInstance')) {
 }
 
 if (! function_exists('LC_CurrentConfig')) {
-    function LC_CurrentConfig($key)
+    function LC_CurrentConfig($key, $default = null)
     {
-        if ($currentConfig = config('livecms.current_config.'.$key)) {
+        if ($currentConfig = config('livecms.current_config.'.$key, $default)) {
             return $currentConfig;
         }
 
         $currentConfig = config('livecms.instances.'.LC_CurrentInstance());
         config(['livecms.current_config' => $currentConfig]);
-        return $currentConfig[$key];
+        return $currentConfig[$key] ?? $default;
+    }
+}
+
+if (! function_exists('LC_Guard')) {
+    function LC_Guard()
+    {
+        $instance = LC_CurrentInstance();
+        if ($guard = config('livecms.instances.'.$instance.'.guard')) {
+            return $guard;
+        }
+        return config('livecms.guard');
+    }
+}
+
+if (! function_exists('LC_GuardName')) {
+    function LC_GuardName()
+    {
+        $guard = LC_Guard();
+        return is_string($guard) ? $guard : $guard['name'];
+    }
+}
+
+if (! function_exists('LC_Middleware')) {
+    function LC_Middleware($group, $instance = null)
+    {
+        $middlewares = [];
+        $wrapped = config('livecms.middleware.wrapped.livecms.'.$group, []);
+        foreach ($wrapped as $key => $middleware) {
+            if (array_last(explode('.', $middleware)) == (string) $key) {
+                $middlewares[] = $middleware.($instance ? ':'.$instance : '');
+            } else {
+                $middlewares[] = $middleware;
+            }
+        }
+
+        if ($instance) {
+            $wrapped = config('livecms.middleware.wrapped.'.$instance.'.'.$group, []);
+            foreach ($wrapped as $middleware) {
+                $middlewares[] = $middleware;
+            }
+        }
+
+        return $middlewares;
     }
 }
 
@@ -35,6 +80,32 @@ if (! function_exists('LC_Route')) {
         return route(LC_BaseRoute().'.'.$routeName);
     }
 }
+
+if (! function_exists('LC_Session')) {
+    function LC_Session($key = null, $default = null)
+    {
+        if (is_null($key)) {
+            return app('session');
+        }
+
+        $key = LC_GuardName().'_sessions';
+
+        if (is_array($key)) {
+            return app('session')->put($key);
+        }
+
+        return app('session')->get($key, $default);
+    }
+}
+
+if (! function_exists('LC_DestroySession')) {
+    function LC_DestroySession()
+    {
+        $key = LC_GuardName().'_sessions';
+        return app('session')->forget($key);
+    }
+}
+
 
 if (! function_exists('LC_CurrentTheme')) {
     function LC_CurrentTheme()
