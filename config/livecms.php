48ea2779dfd191738e6083841aad20c0b4989aa4
Mokhamad Rofiudin <mokh.rofiudin@gmail.com>
2018-09-30 17:16:03 +0700

    update config

--- config/livecms.php
+++ config/livecms.php
@@ -6,32 +6,49 @@ return [
 
         // You can make multiple instances
         'main' => [
+            // Name of instance
+            'name' => 'Live CMS',
+
+            'slogan' => 'Cool CMS For Laravel',
+
             // Theme
             'theme' => 'gantelella',
 
             // base url
             'base_url' => env('CMS_URL', 'http://localhost'),
 
-            // URI to access CMS
-            'uris' => [
-                // login uri
-                'login' => 'login',
-            ],
+            'auth_uri' => null,
+
+            'allow_register' => true,
+
+            'verify_email' => true,
+
+            // You can use your specific guard, just the same format with default guard or just the same
+            // use null if remain the same
+            // Define instance own guard, to deal with different login session
+            'guard' => null,
 
             'middleware' => [
+                // list of web middleware
+                'web' => [
+                    // custom guest middleware for this instance
+                ],
+
+                // list of api middleware
+                'api' => [
+                    // custom guest middleware for this instance
+                ],
+
                 // list of guest middleware
                 'guest' => [
-                    LiveCMS\Middleware\RedirectIfAuthenticated::class,
+                    // custom guest middleware for this instance
                 ],
 
                 // list of auth middleware
                 'auth' => [
-                    LiveCMS\Middleware\Authenticate::class,
+                    // custom auth middleware for this instance
                 ],
             ],
-
-            // You can use your own model
-            'user_model' => LiveCMS\User\UserModel::class,
 
             // resources
             'resources' => [
@@ -42,7 +59,7 @@ return [
         // You can name it 'second' or whatever you want
         'second' => [
             //
-        ]
+        ],
     ],
 
     'template_path' => resource_path('livecms/templates'),
@@ -53,6 +70,7 @@ return [
     // 'view_path' => resource_path('livecms/views'),
     'view_path' => __DIR__.'/../views',
 
+    // you can use guard that is defined in auth config or create the new one here.
     'guard' => [
         // guard name
         'name' => 'livecms',
@@ -60,7 +78,33 @@ return [
         // Guard provider
         'provider' => [
             'driver' => 'eloquent',
-            'model' => LiveCMS\User\UserModel::class,
+            'model' => LiveCMS\Auth\UserModel::class,
         ],
     ],
+
+    'middleware' => [
+        // web
+        'web' => [
+            'web', // web is name of middleware group defined by laravel, see App/Http/Kernel.php file.
+            LiveCMS\Middleware\Middleware::class,
+        ],
+
+        // Api
+        'api' => [
+            //
+        ],
+
+        // list of default guest middleware
+        'guest' => [
+            LiveCMS\Middleware\RedirectIfAuthenticated::class,
+        ],
+
+        // list of default auth middleware
+        'auth' => [
+            LiveCMS\Middleware\Authenticate::class,
+        ],
+    ],
+
+    // set true if you only want to logout specific instance not global logout.
+    'independent_logout' => true,
 ];
