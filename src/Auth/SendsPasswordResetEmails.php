957e72479a91a8854210e517df58942823347a5e
Mokhamad Rofiudin <mokh.rofiudin@gmail.com>
2018-10-03 05:21:49 +0700

    add custom Password Broker issue #6

---
+++ src/Auth/SendsPasswordResetEmails.php
@@ -0,0 +1,138 @@
+<?php
+
+namespace LiveCMS\Auth;
+
+use Illuminate\Auth\Passwords\DatabaseTokenRepository;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Password;
+use Illuminate\Support\Str;
+
+trait SendsPasswordResetEmails
+{
+    protected function getUser($credentials = [])
+    {
+        return $this->userModel->newQuery()->where($credentials)->first();
+    }
+
+    /**
+     * Display the form to request a password reset link.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function showLinkRequestForm()
+    {
+        return view('livecms::auth.reset');
+    }
+
+    /**
+     * Validate the email for the given request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return void
+     */
+    protected function validateEmail(Request $request)
+    {
+        $this->validate($request, ['email' => 'required|email']);
+    }
+
+   /**
+     * Create a token repository instance based on the given configuration.
+     *
+     * @return \Illuminate\Auth\Passwords\TokenRepositoryInterface
+     */
+    protected function tokens()
+    {
+        $name = config('auth.defaults.passwords');
+        $config = config('auth.passwords.'.$name);
+
+        $key = config('app.key');
+
+        if (Str::startsWith($key, 'base64:')) {
+            $key = base64_decode(substr($key, 7));
+        }
+
+        $connection = $config['connection'] ?? null;
+
+        return new DatabaseTokenRepository(
+            app('db')->connection($connection),
+            app('hash'),
+            $config['table'],
+            $key,
+            $config['expire']
+        );
+    }
+
+    /**
+     * Send a reset link to the given user.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\RedirectResponse|\Illuminate\Http\JsonResponse
+     */
+    public function sendResetLinkEmail(Request $request)
+    {
+        $this->validateEmail($request);
+
+        // We will send the password reset link to this user. Once we have attempted
+        // to send the link, we will examine the response then see the message we
+        // need to show to the user. Finally, we'll send out a proper response.
+        $response = $this->sendResetLink(
+            $request->only('email')
+        );
+
+        return $response == Password::RESET_LINK_SENT
+                    ? $this->sendResetLinkResponse($response)
+                    : $this->sendResetLinkFailedResponse($request, $response);
+    }
+
+    /**
+     * Send a password reset link to a user.
+     *
+     * @param  array  $credentials
+     * @return string
+     */
+    protected function sendResetLink(array $credentials)
+    {
+        // First we will check to see if we found a user at the given credentials and
+        // if we did not we will redirect back to this current URI with a piece of
+        // "flash" data in the session to indicate to the developers the errors.
+        $user = $this->getUser($credentials);
+
+        if (is_null($user)) {
+            return Password::INVALID_USER;
+        }
+
+        // Once we have the reset token, we are ready to send the message out to this
+        // user with a link to reset their password. We will then redirect back to
+        // the current URI having nothing set in the session to indicate errors.
+        $user->sendPasswordResetNotification(
+            $this->tokens()->create($user)
+        );
+
+        return Password::RESET_LINK_SENT;
+    }
+
+    /**
+     * Get the response for a successful password reset link.
+     *
+     * @param  string  $response
+     * @return \Illuminate\Http\RedirectResponse|\Illuminate\Http\JsonResponse
+     */
+    protected function sendResetLinkResponse($response)
+    {
+        return back()->with('status', trans($response));
+    }
+
+    /**
+     * Get the response for a failed password reset link.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  string  $response
+     * @return \Illuminate\Http\RedirectResponse|\Illuminate\Http\JsonResponse
+     */
+    protected function sendResetLinkFailedResponse(Request $request, $response)
+    {
+        return back()->withErrors(
+            ['email' => trans($response)]
+        );
+    }
+}
