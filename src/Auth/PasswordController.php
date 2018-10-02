6fc2dbfda9d24bd2250d74ce7d7bcd5799ac0283
Mokhamad Rofiudin <mokh.rofiudin@gmail.com>
2018-10-03 05:43:08 +0700

    update controllers issue #15

--- src/Auth/PasswordController.php
+++ src/Auth/PasswordController.php
@@ -3,9 +3,47 @@
 namespace LiveCMS\Auth;
 
 use Illuminate\Foundation\Auth\ResetsPasswords;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use LiveCMS\Controllers\Controller;
 
 class PasswordController extends Controller
 {
     use ResetsPasswords;
+
+    /**
+     * Display the password reset view for the given token.
+     *
+     * If no token is present, display the link request form.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  string|null  $token
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
+    public function showResetForm(Request $request, $token = null)
+    {
+        return view('livecms::auth.password')->with(
+            ['token' => $token, 'email' => $request->email]
+        );
+    }
+
+    /**
+     * Get the post register / login redirect path.
+     *
+     * @return string
+     */
+    public function redirectPath()
+    {
+        return LC_Route('index');
+    }
+
+    /**
+     * Get the guard to be used during authentication.
+     *
+     * @return \Illuminate\Contracts\Auth\StatefulGuard
+     */
+    protected function guard()
+    {
+        return Auth::guard(LC_GuardName());
+    }
 }
