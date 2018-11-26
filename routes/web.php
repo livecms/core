226bf3dae67277a8d5a2816218f38a54abbfaf89
Mokhamad Rofiudin <mokh.rofiudin@gmail.com>
2018-11-26 09:34:15 +0700

    add media library controller

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,44 @@ return function ($name, $instance) {
         return view('livecms::home');
     })->name('index');
 
+    Route::group(['prefix' => 'media-library', 'as' => 'media-library.'], function() {
+        Route::get(
+            'open/{editor?}',
+            [
+                'as' => 'open',
+                'uses' => '\LiveCMS\MediaLibrary\MediaLibraryController@open'
+            ]
+        );
+        Route::get(
+            '{type}/get/{limit?}',
+            [
+                'as' => 'get',
+                'uses' => '\LiveCMS\MediaLibrary\MediaLibraryController@get'
+            ]
+        );
+        Route::post(
+            '{type}/upload',
+            [
+                'as' => 'upload',
+                'uses' => '\LiveCMS\MediaLibrary\MediaLibraryController@upload'
+            ]
+        );
+        Route::put(
+            '{type}/{identifier}/rename',
+            [
+                'as' => 'rename',
+                'uses' => '\LiveCMS\MediaLibrary\MediaLibraryController@rename'
+            ]
+        );
+        Route::delete(
+            '{type}/{identifier}/delete', 
+            [
+                'as' => 'delete',
+                'uses' => '\LiveCMS\MediaLibrary\MediaLibraryController@delete'
+            ]
+        );
+    });
+
     foreach ($instance['resources'] as $resource) {
         $resource::$baseRoute = 'livecms.'.$name;
         $resource::register();
